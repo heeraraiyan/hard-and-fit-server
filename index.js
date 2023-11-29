8928ccdd5bd4e049c51d95b11cde923108b42c2e
heeraraiyan <heeraraiyan07@yahoo.com>
2023-11-29 12:51:48 +0600

    post operation done for users

--- index.js
+++ index.js
@@ -31,6 +31,20 @@ async function run() {
 
     const featureCollection = client.db("hardDb").collection("features");
     const trainerCollection = client.db("hardDb").collection("trainers");
+    const userCollection = client.db("hardDb").collection("users");
+
+    // user related 
+
+    app.post('/users', async (req,res) =>{
+      const user =req.body;
+      const query ={ email: user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({ message: 'user already exist' , insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
 
     app.get('/features', async(req, res) =>{
         const result = await featureCollection.find().toArray();
