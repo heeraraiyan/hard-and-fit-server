4d23fa62caa47ffdee90af440e054bee208f4460
heeraraiyan <heeraraiyan07@yahoo.com>
2023-11-30 17:08:39 +0600

    vercel file added for deploy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
+
 const cors = require('cors');
 require('dotenv').config()
+const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middleware 
@@ -12,7 +14,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fwkvcsa.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const featureCollection = client.db("hardDb").collection("features");
     const trainerCollection = client.db("hardDb").collection("trainers");
@@ -99,8 +101,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
